5ed044cfc44cd86718836553009626b1211eb8eb
Minoo Yaghoubi <155952014+Minoo-YH@users.noreply.github.com>
2026-09-20 01:51:43 +0300

    Add Kelvin to Celsius conversion

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -11,4 +11,8 @@ public class TemperatureConverter {
         return celsius < -40 || celsius > 50;
     }
 
+    public double kelvinToCelsius(double kelvin) {
+        return kelvin -273.15;
+    }
+
 }
